2400ab6ca6fb8a676d15f5fc51afdad42f6fb015
NastyaSiryk <>
2023-10-18 18:47:46 +0200

    ewrdhggh,m

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-let arrayy = [1, 2, 3, 4]
+var requestURL = 'https://paskudacoding.github.io/test/text.json'
+var request = new XMLHttpRequest(requestURL)
+request.open('GET', requestURL)
+request.responseType = "json"
+request.send()
+request.onload = function(){
+    var
+}
+
+
+
+let array = [1, 2, 3, 4, 5]
 
