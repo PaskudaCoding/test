88fc1300279b7b57687ca2fdc371c66d18bd381d
NastyaSiryk <>
2023-10-18 18:54:28 +0200

    vgfjgpoih

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ request.open('GET', requestURL)
 request.responseType = "json"
 request.send()
 request.onload = function(){
-    var array = request.response
-    console.log(array)
+    var responseText = request.response
+    var response = JSON.parse(responseText)
+    console.log(response)
+    console.log('-----------------------------')
 }
 
+let array = [1, 2, 3, 4, 5]
 
-
-// let array = [1, 2, 3, 4, 5]
+request.open('GET', requestURL)
+request.responseType = "text"
+request.send()
 
